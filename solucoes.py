658f8275683e7a9d60d268e1b59984d8e21854a2
Fernanda Giacobbo <fernanda.giacobbo@pucpr.edu.br>
2025-09-03 20:53:08 -0300

    feat: implementa logica da funcao anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,8 +1,16 @@
 def sao_anagramas(string1, string2):
-  # TODO: Implementar a lógica
-  pass
+    s1 = ''.join(string1.lower().split())
+    s2 = ''.join(string2.lower().split())
+
+    return sorted(s1) == sorted(s2)
+
+print(sao_anagramas("caro", "arco"))
+print(sao_anagramas("luar", "raul"))
+print(sao_anagramas("vida", "mesa"))
+    
+pass
 
 def cifra_de_cesar(texto, deslocamento):
-  # TODO: Implementar a lógica
+  
   pass
   
